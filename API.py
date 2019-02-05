8c9469f86c6b3572e440cb9add05ef3de42a2afb
Kevin Ellis <ellisk@mit.edu>
2019-02-04 19:32:08 -0500

    API progress

---
+++ API.py
@@ -0,0 +1,13 @@
+class Solver:
+    def __init__(self):
+        pass
+
+    def infer(self, spec, loss, timeout):
+        """
+        spec: specification of goal
+        timeout: maximum time to run solver, measured in seconds
+        returns: list of (program, loss, time)
+        Should take no longer than timeout seconds."""
+        assert False, "not implemented"
+
+        
