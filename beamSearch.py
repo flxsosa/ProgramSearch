b0e3e20af7548060b700d838dccf2a9a357c95a5
Kevin Ellis <ellisk@mit.edu>
2019-03-05 12:24:03 -0500

    beam search

---
+++ beamSearch.py
@@ -0,0 +1,42 @@
+from API import *
+from programGraph import *
+
+class BeamSearch(Solver):
+    def __init__(self, model):
+        self.model = model
+
+    def _infer(self, spec, loss, timeout):
+
+        specEncoding = self.model.specEncoder(spec)
+
+        objectEncodings = ScopeEncoding(self.model)
+
+        B = 50
+
+        class Particle():
+            def __init__(self, graph, ll, finished=False):
+                self.graph = graph
+                self.ll = ll
+                self.finished = finished
+                self.reported = False
+
+        population = [Particle(ProgramGraph([]), 0.)]
+
+        while any( not p.finished for p in population ):
+            children = []
+            for p in particles:
+                if p.finished: continue
+                
+                for o, l in self.model.beamNextLine(spec, specEncoding, p.graph, objectEncodings, B):
+                    if o is None:
+                        children.append(Particle(p.graph, p.ll + l, finished=True))
+                    else:
+                        children.append(Particle(p.graph.extend(o), p.ll + l))
+
+            children.sort(key=lambda p: -p.ll)
+            population = children[:B]
+
+            for p in children:
+                if p.finished and not p.reported:
+                    self._report(p.graph)
+                    p.reported = True
