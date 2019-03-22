c129ef8c889641484f562edcf0e80af45b8e3efb
evanthebouncy <evanthebouncy@gmail.com>
2019-03-22 15:53:19 -0400

    YO BUTT

--- ROBUT.py
+++ ROBUT.py
@@ -6,7 +6,7 @@ import string
 from string import printable
 import re
 import pregex as pre
-from ROB import R, _INDEX, _DELIMITER
+from ROB import R, _INDEX, _DELIMITER, _CHARACTER
 
 N_IO = 5
 
@@ -145,7 +145,6 @@ class SubStr1(Button):
                         pstate.committed,
                         pstate.outputs,
                         pstate.past_buttons + [self])
-        ret.substrk1 = self.k1
         return ret
 
 class SubStr2(Button):
@@ -161,7 +160,8 @@ class SubStr2(Button):
         self.k2 = k2
 
     def __call__(self, pstate):
-        k1 = pstate.substrk1
+        # get the k1 from the previous button
+        k1 = pstate.past_buttons[-1].k1
         scratch_new = [x[k1:self.k2] for x in pstate.scratch]
         return RobState(pstate.inputs,
                         scratch_new,
@@ -169,22 +169,39 @@ class SubStr2(Button):
                         pstate.outputs,
                         pstate.past_buttons + [self])
 
-class GetToken(Button):
+class GetToken1(Button):
 
     @staticmethod
     def generate_buttons():
-        return [GetToken(name, i) \
-                for name in R.possible_types.keys() for i in _INDEX] 
+        return [GetToken1(name) for name in R.possible_types.keys()] 
 
-    def __init__(self, rname, i):
-        self.name = f"GetToken({rname}, {i})"
+    def __init__(self, rname):
+        self.name = f"GetToken1({rname})"
         self.rname = rname
         self.t = R.possible_types[rname]
-        self.i = i
-        self.f = lambda x: re.findall(self.t[0], x)[i]
 
     def __call__(self, pstate):
-        scratch_new = [self.f(x) for x in pstate.scratch]
+        return  RobState(pstate.inputs,
+                         pstate.scratch,
+                         pstate.committed,
+                         pstate.outputs,
+                         pstate.past_buttons + [self])
+
+class GetToken2(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [GetToken2(i) for i in _INDEX] 
+
+    def __init__(self, i):
+        self.name = f"GetToken2({i})"
+        self.i = i
+        self.f = lambda x, t: re.findall(t[0], x)[i]
+
+    def __call__(self, pstate):
+        # get the type from GetToken1 button
+        t = pstate.past_buttons[-1].t
+        scratch_new = [self.f(x, t) for x in pstate.scratch]
         return RobState(pstate.inputs,
                         scratch_new,
                         pstate.committed,
@@ -235,22 +252,38 @@ class GetFrom(Button):
                         pstate.outputs,
                         pstate.past_buttons + [self])
 
-class GetFirst(Button):
+class GetFirst1(Button):
 
     @staticmethod
     def generate_buttons():
-        return [GetFirst(name, i) \
-        for name in R.possible_types.keys() for i in _INDEX] 
+        return [GetFirst1(name) for name in R.possible_types.keys()]
 
-    def __init__(self, rname, i):
-        self.name = f"GetFirst({rname}, {i})"
+    def __init__(self, rname):
+        self.name = f"GetFirst1({rname})"
         self.rname = rname
         self.t = R.possible_types[rname]
-        self.i = i
-        self.f = lambda string: ''.join(re.findall(self.t[0], string)[:self.i])
 
     def __call__(self, pstate):
-        scratch_new = [self.f(x) for x in pstate.scratch]
+        return RobState(pstate.inputs,
+                        pstate.scratch,
+                        pstate.committed,
+                        pstate.outputs,
+                        pstate.past_buttons + [self])
+
+class GetFirst2(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [GetFirst2(i) for i in _INDEX] 
+
+    def __init__(self, i):
+        self.name = f"GetFirst2({i})"
+        self.i = i
+        self.f = lambda string, t: ''.join(re.findall(t[0], string)[:self.i])
+
+    def __call__(self, pstate):
+        t = pstate.past_buttons[-1].t
+        scratch_new = [self.f(x, t) for x in pstate.scratch]
         return RobState(pstate.inputs,
                         scratch_new,
                         pstate.committed,
@@ -278,17 +311,163 @@ class GetAll(Button):
                         pstate.outputs,
                         pstate.past_buttons + [self])
 
+class GetSpan1(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [GetSpan1(name) for name in R.possible_r.keys()]
+
+    def __init__(self, rname):
+        self.name = f"GetSpan1({rname})"
+        self.rname = rname
+        self.r1 = R.possible_r[rname]
+
+    def __call__(self, pstate):
+        return RobState(pstate.inputs,
+                        pstate.scratch,
+                        pstate.committed,
+                        pstate.outputs,
+                        pstate.past_buttons + [self])
+
+class GetSpan2(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [GetSpan2(i) for i in _INDEX] 
+
+    def __init__(self, i1):
+        self.name = f"GetSpan2({i1})"
+        self.i1 = i1
+
+    def __call__(self, pstate):
+        return RobState(pstate.inputs,
+                        pstate.scratch,
+                        pstate.committed,
+                        pstate.outputs,
+                        pstate.past_buttons + [self])
+
+class GetSpan3(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [GetSpan3("Start"), GetSpan3("End")]
+
+    def __init__(self, b1):
+        self.name = f"GetSpan3({b1})"
+        self.b1 = b1
+
+    def __call__(self, pstate):
+        return RobState(pstate.inputs,
+                        pstate.scratch,
+                        pstate.committed,
+                        pstate.outputs,
+                        pstate.past_buttons + [self])
+
+class GetSpan4(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [GetSpan4(name) for name in R.possible_r.keys()]
+
+    def __init__(self, rname):
+        self.name = f"GetSpan4({rname})"
+        self.rname = rname
+        self.r2 = R.possible_r[rname]
+
+    def __call__(self, pstate):
+        return RobState(pstate.inputs,
+                        pstate.scratch,
+                        pstate.committed,
+                        pstate.outputs,
+                        pstate.past_buttons + [self])
+
+class GetSpan5(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [GetSpan5(i) for i in _INDEX] 
+
+    def __init__(self, i2):
+        self.name = f"GetSpan5({i2})"
+        self.i2 = i2
+
+    def __call__(self, pstate):
+        return RobState(pstate.inputs,
+                        pstate.scratch,
+                        pstate.committed,
+                        pstate.outputs,
+                        pstate.past_buttons + [self])
+
+class GetSpan6(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [GetSpan6("Start"), GetSpan6("End")]
+
+    def __init__(self, b2):
+        self.name = f"GetSpan6({b2})"
+        self.b2 = b2
+
+        def f(input_str, r1, i1, b1, r2, i2, b2):
+            """
+            all complaints of this function please send to mnye@mit.edu
+            evanthebouncy took no part in this :v
+            """
+            return input_str[[m.end() for m in re.finditer(r1[0], input_str)][i1] if b1 == "End" else [m.start() for m in re.finditer(r1[0], input_str)][i1] : [m.end() for m in re.finditer(r2[0], input_str)][i2] if b2 == "End" else [m.start() for m in re.finditer(r2[0], input_str)][i2]]
+
+        self.f = f
+
+    def __call__(self, pstate):
+        r1 = pstate.past_buttons[-5].r1
+        i1 = pstate.past_buttons[-4].i1
+        b1 = pstate.past_buttons[-3].b1
+        r2 = pstate.past_buttons[-2].r2
+        i2 = pstate.past_buttons[-1].i2
+        b2 = self.b2
+        scratch_new = [self.f(x, r1, i1, b1, r2, i2, b2) for x in pstate.scratch]
+        return RobState(pstate.inputs,
+                        scratch_new,
+                        pstate.committed,
+                        pstate.outputs,
+                        pstate.past_buttons + [self])
+
+class Const(Button):
+
+    @staticmethod
+    def generate_buttons():
+        return [Const(c) for c in _CHARACTER]
+
+    def __init__(self, c):
+        self.name = f"Const({c})"
+        self.c = c
+
+    def __call__(self, pstate):
+        scratch_new = [self.c for x in pstate.scratch]
+        return RobState(pstate.inputs,
+                        scratch_new,
+                        pstate.committed,
+                        pstate.outputs,
+                        pstate.past_buttons + [self])
 
 ALL_BUTTS = ToCase.generate_buttons() +\
             Replace1.generate_buttons() +\
             Replace2.generate_buttons() +\
             SubStr1.generate_buttons() +\
             SubStr2.generate_buttons() +\
-            GetToken.generate_buttons() +\
+            GetToken1.generate_buttons() +\
+            GetToken2.generate_buttons() +\
             GetUpTo.generate_buttons() +\
             GetFrom.generate_buttons() +\
-            GetFirst.generate_buttons() +\
-            GetAll.generate_buttons()
+            GetFirst1.generate_buttons() +\
+            GetFirst2.generate_buttons() +\
+            GetAll.generate_buttons() +\
+            GetSpan1.generate_buttons() +\
+            GetSpan2.generate_buttons() +\
+            GetSpan3.generate_buttons() +\
+            GetSpan4.generate_buttons() +\
+            GetSpan5.generate_buttons() +\
+            GetSpan6.generate_buttons() +\
+            Const.generate_buttons()
 
 
 # ===================== UTILS ======================
@@ -322,11 +501,12 @@ def test2():
     pstate = RobState.new(["Mr.Pu", "Mr.Poo"],
                           ["Pu", "Poo"])
     fs = [
-            GetToken("Word", 1),
+            GetToken1("Word"),
+            GetToken2(1),
             Commit(),
             ]
     print (apply_fs(pstate, fs))
-
+    
     gs = [
             GetUpTo("."),
             Commit(),
@@ -340,10 +520,13 @@ def test2():
     print (apply_fs(pstate, hs))
 
     ts = [
-            GetFirst("Word", 2),
+            GetFirst1("Word"),
+            GetFirst2(2),
             Commit(),
             ]
     print (apply_fs(pstate, ts))
+
+    assert 0
 
     vs = [
             GetAll("Word"),
@@ -351,6 +534,27 @@ def test2():
             ]
     print (apply_fs(pstate, vs))
 
+def test3():
+    print (ALL_BUTTS)
+    print (len(ALL_BUTTS))
+    pstate = RobState.new(["(hello)123", "(mister)123"],
+                          ["1234", "4"])
+    fs = [
+            GetSpan1("("),
+            GetSpan2(0),
+            GetSpan3("End"),
+            GetSpan4(")"),
+            GetSpan5(0),
+            GetSpan6("Start"),
+            ToCase("AllCaps"),
+            Commit(),
+            Const("R"),
+            Commit(),
+            Const("E"),
+            Commit(),
+            ]
+    print (apply_fs(pstate, fs))
+
 if __name__ == '__main__':
-    test2()
+    test3()
 
