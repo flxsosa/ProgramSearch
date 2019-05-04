abe23dc0d3c65debc1989e970b08f6237c5c8689
evanthebouncy <evanthebouncy@gmail.com>
2019-05-04 16:27:38 -0400

    tools done

--- tool_set.py
+++ tool_set.py
@@ -1,6 +1,5 @@
 from CAD import *
 
-
 """basically all of this is broken"""
 
 if __name__ == '__main__':
@@ -20,103 +19,91 @@ if __name__ == '__main__':
                          x + h,y + h)
     def circle(x,y,d):
         return Circle(x*2,y*2,d)
-    # wrench 
-    def wrench():
-        s1 = tool.Rect(8, 9, 2, 8, 0)
-        s2 = tool.Circ(8, 4, 5)
-        s3 = tool.Rect(7, 3, 3, 2, 1)
-        s = s1 + (s2 - s3)
-        scipy.misc.imsave('demo/wrench.png', s.to_np())
+
+    # wrench
     def wrench():
         s1 = rectangle(8, 9, 2, 8)
         s2 = circle(8, 4, 10)
-        s3 = slanted(8,4,1,3)
+        s3 = slanted(5,0,1,3)
         return s1 + s2 - s3
 
-    wrench().export("demo/whatever.png",256)
-    assert False
+    wrench().export("demo/wrench.png",256)
 
     # shovel 
     def shovel():
-        s1 = tool.Rect(8, 7, 1, 8, 0)
-        s2 = tool.Circ(8, 13, 3)
-        s3 = tool.Rect(8, 3, 3, 1, 0)
-        s4 = tool.Rect(8, 12, 3, 2, 0)
-        s = s1 + s2 + s3 + s4
-        scipy.misc.imsave('demo/shovel.png', s.to_np())
-    shovel()
+        s1 = rectangle(8, 7, 1, 8)
+        s2 = circle(8, 13, 6)
+        s3 = rectangle(8, 3, 3, 1)
+        s4 = rectangle(8, 12, 3, 2)
+        return s1 + s2 + s3 + s4
+
+    shovel().export("demo/shovel.png",256)
 
     # key 
     def key():
-        s1 = tool.Rect(7, 8, 1, 8, 2)
-        s2 = tool.Circ(12, 8, 4)
-        s3 = tool.Circ(12, 8, 2)
-        s4 = tool.Rect(5, 7, 3, 2, 0)
-        s5 = tool.Rect(5, 7, 1, 3, 0)
-        s = s1 + (s2 - s3) + (s4 - s5)
-        scipy.misc.imsave('demo/key.png', s.to_np())
-    key()
+        s1 = rectangle(8, 8, 8, 1)
+        s2 = circle(13, 8, 8)
+        s3 = circle(13, 8, 4)
+        s4 = rectangle(5, 8, 3, 2)
+        s5 = rectangle(5, 8, 1, 2)
+        return s1 + (s2 - s3) + (s4 - s5)
 
-    # Ibeam 
-    def Ibeam():
-        s1 = tool.Rect(8, 8, 2, 8, 0)
-        s2 = tool.Rect(8, 4, 5, 2, 0)
-        s3 = tool.Rect(8, 12, 5, 2, 0)
-        s = s1 + s2 + s3
-        scipy.misc.imsave('demo/Ibeam.png', s.to_np())
-    Ibeam()
+    key().export("demo/key.png",256)
 
-    # dumbell 
-    def dumbell():
-        s1 = tool.Rect(8, 8, 2, 8, 2)
-        s2 = tool.Circ(4, 8, 6)
-        s3 = tool.Circ(12, 8, 6)
-        s4 = tool.Rect(6, 8, 3, 6, 0)
-        s5 = tool.Rect(10, 8, 3, 6, 0)
-        s = s1 + (s2 - s4) + (s3 - s5)
-        scipy.misc.imsave('demo/dumbell.png', s.to_np())
-    dumbell()
+    # hammer 
+    def hammer():
+        s1 = rectangle(9, 8, 6, 2)
+        s2 = rectangle(5, 8, 3, 5)
+        return s1 + s2
+
+    hammer().export("demo/hammer.png",256)
+
+    # plier 
+    def plier():
+        s1 = rectangle(6, 8, 8, 1)
+        s2 = slanted(10, 8, 5, 1)
+        s3 = circle(12, 8, 8)
+        s4 = rectangle(14, 6, 3, 3)
+        return s1 + s2 + (s3 - s4)
+
+    plier().export("demo/plier.png",256)
 
     # double_wrench 
     def double_wrench():
-        s1 = tool.Rect(8, 8, 1, 10, 3)
-        s2 = tool.Circ(4, 4, 3)
-        s3 = tool.Rect(3, 3, 1, 2, 3)
-        s4 = tool.Circ(12, 12, 3)
-        s5 = tool.Rect(12, 12, 1, 1, 1)
-        s = s1 + (s2 - s3) + (s4 - s5)
-        scipy.misc.imsave('demo/double_wrench.png', s.to_np())
-    double_wrench()
+        s1 = slanted(11, 4, 7, 1)
+        s2 = circle(12, 4, 6)
+        s3 = rectangle(12, 4, 1, 1)
+        s4 = circle(4, 12, 6)
+        s5 = rectangle(4, 13, 1, 2)
+        return s1 + (s2 - s3) + (s4 - s5)
+
+    double_wrench().export("demo/double_wrench.png",256)
 
     # mag 
     def mag():
-        s1 = tool.Rect(11, 11, 1, 3, 3)
-        s2 = tool.Circ(8, 8, 7)
-        s3 = tool.Circ(8, 8, 5)
-        s = s1 + (s2 - s3)
-        scipy.misc.imsave('demo/mag.png', s.to_np())
-    mag()
+        s1 = circle(8, 8, 20)
+        s2 = circle(8, 8, 16)
+        s3 = slanted(11, 11, 1, 3)
+        return (s1 - s2) + s3
+
+    mag().export("demo/mag.png",256)
 
     # sickle 
     def sickle():
-        s1 = tool.Rect(5, 11, 1, 3, 1)
-        s2 = tool.Circ(8, 7, 8)
-        s3 = tool.Circ(7, 7, 7)
-        s = s1 + (s2 - s3)
-        scipy.misc.imsave('demo/sickle.png', s.to_np())
-    sickle()
+        s1 = circle(8, 7, 20)
+        s2 = circle(7, 7, 18)
+        s3 = slanted(5, 10, 3, 1)
+        return (s1 - s2) + s3
+
+    sickle().export("demo/sickle.png",256)
 
     # comrad 
     def comrad():
-        s1 = tool.Rect(5, 11, 1, 3, 1)
-        s2 = tool.Circ(8, 7, 8)
-        s3 = tool.Circ(7, 7, 7)
-        ss = s1 + (s2 - s3)
+        s1 = circle(8, 7, 22)
+        s2 = circle(7, 7, 20)
+        s3 = slanted(5, 10, 3, 1)
+        s4 = slanted(8, 7, 1, 6)
+        s5 = slanted(8, 5, 3, 2)
+        return (s1 - s2) + s3 + s4 + s5
 
-        s4 = tool.Rect(10, 10, 1, 6, 3)
-        s5 = tool.Rect(7, 7, 3, 2, 3)
-        sh = s4 + s5
-        
-        s = ss + sh
-        scipy.misc.imsave('demo/comrad.png', s.to_np())
-    comrad()
+    comrad().export("demo/comrad.png",256)
