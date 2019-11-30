46b3e4d32b6578ec92d12a5e1a78fcca4c0a5ff3
Maxwell Nye <maxnye@comcast.net>
2019-11-30 18:18:36 -0500

    test and plot.py

---
+++ test_and_plot.py
@@ -0,0 +1,30 @@
+import os
+from datetime import datetime
+
+#TODO add timeout stuff so can rerun
+timestamp = datetime.now().strftime('%FT%T')
+timestamp ='2019-11-30T17:21:39'
+
+checkpoints = [
+('abs_modular', '2d_imitation_abstraction_2019-11-23T17:53:35.pickle', 'SMC'),
+('abs_modular_rl' , '2d_critic_abstraction_2019-11-22T11:42:53.pickle', 'SMC'),
+#('modularRLfinetune', '2d_rl_after_contrastive.pickle'),
+('no_REPL', '2d_imitation_noExecution_abstraction_2019-11-07T10:50:12.pickle', 'noExecution'),
+('abs_non_modular', '2d_imitation_abstraction_2019-11-28T13:21:02.pickle', 'SMC'),
+('full_repl', '2d_critic_2019-11-30T01:01:12.pickle', 'SMC'),
+]
+
+for name, checkpoint, solver in checkpoints:
+	os.system(f"python driver.py test --2d --train_abstraction --checkpoint checkpoints/{checkpoint} --solvers {solver} --timeout 20 --outputFolder {name}{timestamp} --maxShapes 30")
+
+names, _, _ = zip(*checkpoints)
+testResults = " ".join(f"experimentOutputs/{name}{timestamp}/testResults.pickle" for name in names)
+names = " ".join(names)
+
+cmd = f"python graphs.py {testResults} --names {names} -t 20 -e figures/graph{timestamp}.png -n testgraph"
+print(cmd)
+os.system(cmd)
+
+#python driver.py test --2d --train_abstraction --checkpoint checkpoints/2d_critic_2019-11-30T01:01:12.pickle --solvers SMC --timeout 10 --outputFolder fullREPLTEST --maxShapes 20
+#python driver.py test --2d --train_abstraction --checkpoint checkpoints/2d_imitation_noExecution_abstraction_2019-11-07T10:50:12.pickle --solvers noExecution --timeout 20 --outputFolder no_REPL --maxShapes 30
+#sbatch execute_gpu_1080.sh python driver.py imitation --2d --train_abstraction --contrastive --nonmodular --checkpoint nonmodularAbstractContrastiveNoSpecInOE.pickle --trainTime 48
